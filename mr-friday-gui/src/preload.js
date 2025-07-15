a6fd42f6a5b6f030a8d1814419cc1fb3a4b77994
Dharun-prasath <dharunprasath.murugan@gmail.com>
2025-07-15 12:14:50 +0530

    Test

--- mr-friday-gui/src/preload.js
+++
@@ -1,6 +0,0 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-
-// preload.js
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-window.speechSynthesis = speechSynthesis;
